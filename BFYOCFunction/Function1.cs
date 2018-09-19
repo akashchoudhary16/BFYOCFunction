bb3db562fbf4dde16a60a17e4b99e7d38ccb7fe5
akash <akashchoudhary16@outlook.com>
2018-09-19 15:14:29 +0530

    Add project files.

---
+++ BFYOCFunction/Function1.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace BFYOCFunction
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            string product = req.Query["productId"];
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            product = product ?? data?.productId;
+
+            return product != null
+                ? (ActionResult)new OkObjectResult($"The product name for your product id {product} is Starfruit Explosion")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
